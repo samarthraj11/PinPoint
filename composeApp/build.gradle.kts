641adc0ee8c3241f12a2937ae97c525d95286505
samarth.raj <mailforsamarth@gmail.com>
2026-03-12 23:13:31 +0530

    added maps libraries

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -19,6 +19,13 @@ kotlin {
         androidMain.dependencies {
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.activity.compose)
+            dependencies {
+                // Google Maps Compose
+                implementation("com.google.maps.android:maps-compose:4.3.3")
+                implementation("com.google.android.gms:play-services-maps:18.2.0")
+                implementation("com.google.android.gms:play-services-location:21.2.0")
+
+            }
         }
         commonMain.dependencies {
             implementation(libs.compose.runtime)
